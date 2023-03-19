961133b60ac9e122acd425a67989164d8e7cf307
SalauddinAmir <mdsalauddinahmed.amir@gmail.com>
2023-03-19 19:12:25 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// const url =`https://randomuser.me/api/`
+// fetch(url)
+// .then(res=>res.json())
+// .then(data=>console.log(data))
+const handleSearch=()=>{
+     const inputValue = document.getElementById('input_value');
+     const inputItem =inputValue.value;
+
+ if(inputItem){
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputItem}`)
+    .then(response=>response.json())
+    .then(data =>{
+        // console.log(data)
+        if(data.title ==='No Definitions Found'){
+            alert('sorry you have bad luck')
+        }else{
+            displayResult(data)
+        }
+   })
+  }else{
+    alert('sorry you have to valid field')
+  }
+
+      
+     inputValue.value=" ";
+}
+const displayResult =(data)=>{
+    const parent = document.getElementById('diction-container')
+   data[0].phonetics.forEach(element => {
+     
+   });
+
+}
